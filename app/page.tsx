f36a342a4755044cdd9b794e19a18612ebdb24fd
Young2un <popduddms@gmail.com>
2025-05-07 20:01:45 +0900

    [fix]: 메인페이지 use client 제거

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,3 @@
-"use client";
-
-import { useQuery } from "@tanstack/react-query";
-
 export default function Home() {
-  const { data, isLoading } = useQuery<string>({
-    queryKey: ["hello"],
-    queryFn: () =>
-      new Promise((res) => setTimeout(() => res("Hello from TanStack!"), 1000)),
-  });
-
-  return <div>{isLoading ? "Loading..." : data}</div>;
+  return <div>tanstack-query를 실습하는 중입니다</div>;
 }
