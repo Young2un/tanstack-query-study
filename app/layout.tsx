9277984dcd0629a079c4a3d164cabab14366e178
Young2un <popduddms@gmail.com>
2025-05-11 20:35:04 +0900

    docs: 메타데이터 추가

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,14 @@
+import { Metadata } from "next";
 import "./globals.css";
 import ClientProvider from "./providers/ClientProvider";
+
+export const metadata: Metadata = {
+  title: "TanStack Query Playground",
+  description: "비공식 공식문서 데모",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +17,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className="min-h-screen bg-gray-50 px-6 py-12 px-40">
+      <body className="min-h-screen bg-gray-50 py-12 px-40">
         <ClientProvider>{children}</ClientProvider>
       </body>
     </html>
